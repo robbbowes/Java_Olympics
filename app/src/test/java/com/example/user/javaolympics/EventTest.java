8fea877acd4aa9498c81148c04a4aa581c2b8986
Robert James Bowes <up666724@myport.ac.uk>
2017-09-18 09:20:07 +0100

    Winner code seems to work now

--- app/src/test/java/com/example/user/javaolympics/EventTest.java
+++ app/src/test/java/com/example/user/javaolympics/EventTest.java
@@ -34,15 +34,46 @@ public class EventTest {
         ArrayList<Athlete> teamgb = new ArrayList<>();
         teamgb.add(johnsmith);
         teamgb.add(stevenberry);
-        Team team = new Team(3, 4, 1, Country.BRITAIN, teamgb, Discipline.JAVELIN );
+        Team team = new Team(3, 4, 1, Country.BRITAIN, teamgb, Discipline.JAVELIN);
         ArrayList<Competitor> javelinteams = new ArrayList<>();
         javelinteams.add(team);
         Event javelinevent = new Event(Discipline.JAVELIN, javelinteams, 2);
         assertEquals(1, javelinevent.competitors.size());
     }
 
-//    @Test
+    @Test
+    public void canAddAthlete() {
+        Athlete johnsmith = new Athlete(0, 0, 0, Country.BRITAIN, "John Smith", 8, Discipline.JAVELIN);
+        ArrayList<Athlete> teamgb = new ArrayList<>();
+        Event javelinevent = new Event(Discipline.JAVELIN, null, 2);
+        javelinevent.addAthleteToCompetitors(johnsmith);
+        assertEquals(javelinevent.getCompetingTeams().size(), 1);
+    }
 
 
+    @Test
+    public void canGetWinner() {
+        Athlete johnsmith = new Athlete(0, 0, 0, Country.BRITAIN, "John Smith", 8, Discipline.JAVELIN);
+        Athlete stevenberry = new Athlete(0, 0, 0, Country.BRITAIN, "Steven Berry", 8, Discipline.JAVELIN);
+        ArrayList<Athlete> gbathletes = new ArrayList<>();
+        gbathletes.add(johnsmith);
+        gbathletes.add(stevenberry);
+        Team teamgb = new Team(3, 4, 1, Country.BRITAIN, gbathletes, Discipline.JAVELIN );
 
+        Athlete reneeblin = new Athlete(0, 0, 0, Country.FRANCE, "Renée Blin", 7, Discipline.JAVELIN);
+        Athlete francoisberthaud = new Athlete(0, 0, 0, Country.FRANCE, "François Bethaud", 7, Discipline.JAVELIN);
+        ArrayList<Athlete> frathletes = new ArrayList<>();
+        frathletes.add(francoisberthaud);
+        frathletes.add(reneeblin);
+        Team teamfr = new Team(2, 5, 0, Country.FRANCE, frathletes, Discipline.JAVELIN );
+
+        ArrayList<Competitor> javelincompetitors = new ArrayList<>();
+        javelincompetitors.add(teamfr);
+        javelincompetitors.add(teamgb);
+
+        Event javelinevent = new Event(Discipline.JAVELIN, javelincompetitors, 2 );
+
+        assertEquals( teamgb, javelinevent.winner() );
+        assertEquals( 4, teamgb.getGoldMedalCount());
+    }
 }
